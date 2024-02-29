38132eb881874b26d0c04679fa0ea1bb6eda5f48
elidominguezz <160682499+elidominguezz@users.noreply.github.com>
2024-02-29 18:14:35 -0300

    Wordle PPY Elizabeth Dominguez sin APIs

---
+++ Worldle/script.js
@@ -0,0 +1,52 @@
+let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH']
+const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+let intentos = 6;
+
+window.addEventListener('load', init)
+function init(){
+    console.log('Esto se ejecuta solo cuando se carga la pagina web')
+}
+const button = document.getElementById("guess-button");
+button.addEventListener("click", intentar);
+
+function leerIntento(){
+    let intento = document.getElementById("guess-input");
+    intento = intento.value;
+    intento = intento.toUpperCase(); 
+    return intento;
+}
+
+function intentar(){
+    const INTENTO = leerIntento();
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
+    for (let i in palabra){
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if (INTENTO[i]===palabra[i]){ //VERDE
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'green';
+        } else if( palabra.includes(INTENTO[i]) ) { //AMARILLO
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'yellow';
+        } else {      //GRIS
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = 'grey';
+        }
+        ROW.appendChild(SPAN)
+    }
+    GRID.appendChild(ROW)
+    }
+    intentos--
+if (intentos===0){
+    terminar("<h1>PERDISTE!😖</h1>")
+    }
+function terminar(mensaje){
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    BOTON.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+}
+
